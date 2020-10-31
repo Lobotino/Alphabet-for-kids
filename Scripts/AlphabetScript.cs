c72bfe77a0561baf085fa870f2a0f3889e881a9d
Lobotino <Lobotino1@gmail.com>
2020-10-31 18:53:48 +0500

    Добавил букву

---
+++ Scripts/AlphabetScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphabetScript : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    private bool isCollected;
+    
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (isCollected && transform.position.y > 300)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        rb2d.velocity = new Vector2(rb2d.velocity.x, 20);
+        isCollected = true;
+    }
+}
---
+++ Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject player;
+    public Camera playerCamera;
+
+    public GameObject gameOverText;
+    public GameObject playerControllerButtons;
+
+    private bool isGameOver;
+    void Update()
+    {
+        if (!isGameOver && player.transform.position.y < -5)
+        {
+            ShowGameOver(true);
+        }
+    }
+
+    private void ShowGameOver(bool gameOver)
+    {
+        isGameOver = gameOver;
+        gameOverText.SetActive(gameOver);
+        playerControllerButtons.SetActive(!gameOver);
+        playerCamera.transform.parent = null;
+    }
+    
+}
